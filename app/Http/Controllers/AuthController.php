5eba5e547f1578848ef20df9a602de8557c4cc12
DiegoBohorquez05 <judiboor@gmail.com>
2022-12-22 09:15:13 -0500

    Controladores

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,7 +9,7 @@ class AuthController extends Controller
     public function login(Request $request){
         $credentials = $request->only('email','password');
         if(!$token = auth()->attempt($credentials)){
-            return response()->json(['error' => 'Unauthorized', 401]);
+            return response()->json(['error' => 'Unauthorized'], 401);
         }
         return response()->json(['access_token' => $token]);
     }
